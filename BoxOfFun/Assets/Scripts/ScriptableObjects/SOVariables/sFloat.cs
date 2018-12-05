b6ec155e907d4e504f2c0cec62e690f0bea431f6
zubbyonfire <zackbloundele@outlook.com>
2018-12-05 18:00:07 +0000

    Setting up the Scriptable Objects
    Setting up the Event System + Scriptable Object variables

---
+++ BoxOfFun/Assets/Scripts/ScriptableObjects/SOVariables/sFloat.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "sFloat", menuName = "sVariables/sFloat", order = 1)]
+public class sFloat : ScriptableObject, IResetScriptableObject, ISerializationCallbackReceiver
+{
+    //Float value
+    public float value;
+
+    //Can the value be reset in game
+    public bool resettable;
+
+    //When the game starts, the starting value we use (so we can reset if need be)
+    private float startingValue;
+
+    /// <summary>
+    /// Set sFloat value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void SetValue(float _value)
+    {
+        value = _value;
+    }
+
+    /// <summary>
+    /// Set value to another sBool value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void SetValue(sFloat _value)
+    {
+        value = _value.value;
+    }
+
+    /// <summary>
+    /// Add a float value to the value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void AddValue(float _value)
+    {
+        value += _value;
+    }
+
+    /// <summary>
+    /// Add another sFloat value to the value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void AddValue(sFloat _value)
+    {
+        value += _value.value;
+    }
+    
+    /// <summary>
+    /// Recieve callback after unity deseriallzes the object
+    /// </summary>
+    public void OnAfterDeserialize()
+    {
+        startingValue = value;
+    }
+
+    public void OnBeforeSerialize() { }
+
+    /// <summary>
+    /// Reset the value to it's inital value if it's resettable
+    /// </summary>
+    public void ResetValue()
+    {
+        if (resettable)
+        {
+            value = startingValue;
+        }
+    }
+}
---
+++ BoxOfFun/Assets/Scripts/ScriptableObjects/SOVariables/sVector3.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "sFloat", menuName = "sVariables/sVector3", order = 1)]
+public class sVector3 : ScriptableObject, IResetScriptableObject, ISerializationCallbackReceiver
+{
+    //Float value
+    public Vector3 value;
+
+    //Can the value be reset in game
+    public bool resettable;
+
+    //When the game starts, the starting value we use (so we can reset if need be)
+    private Vector3 startingValue;
+
+    /// <summary>
+    /// Set sVector3 value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void SetValue(Vector3 _value)
+    {
+        value = _value;
+    }
+
+    /// <summary>
+    /// Set value to another sVector3 value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void SetValue(sVector3 _value)
+    {
+        value = _value.value;
+    }
+
+    /// <summary>
+    /// Add a Vector3 value to the value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void AddValue(Vector3 _value)
+    {
+        value += _value;
+    }
+
+    /// <summary>
+    /// Add another sVector3 value to the value
+    /// </summary>
+    /// <param name="_value"></param>
+    public void AddValue(sVector3 _value)
+    {
+        value += _value.value;
+    }
+
+    /// <summary>
+    /// Recieve callback after unity deseriallzes the object
+    /// </summary>
+    public void OnAfterDeserialize()
+    {
+        startingValue = value;
+    }
+
+    public void OnBeforeSerialize() { }
+
+    /// <summary>
+    /// Reset the value to it's inital value if it's resettable
+    /// </summary>
+    public void ResetValue()
+    {
+        if (resettable)
+        {
+            value = startingValue;
+        }
+    }
+}
