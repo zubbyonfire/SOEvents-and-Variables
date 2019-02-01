2d340113386b5a016f2698b29dd37fb9390c3631
zubbyonfire <zackbloundele@outlook.com>
2019-02-01 00:28:20 +0000

    Updated Icons + EventManager
    Finished the EventManager and added a tab, which shows any gameObject with an event listener component but with no value assigned.
    
    Begun implementing the custom Scriptable variable icons.
    
    To Do:
    - Finish icons
    - Make Example
    - Tidy up code
    - Add Event debugs

--- BoxOfFun/Assets/SOEvents/Editor/EventManagerWindow.cs
+++ BoxOfFun/Assets/SOEvents/Editor/EventManagerWindow.cs
@@ -19,7 +19,15 @@ namespace ScriptableObjectEvent
 
         //Dictionary of all objects that are using the GameEventListener
         private Dictionary<SOGameEvent, List<GameObject>> gameEventDictionary = new Dictionary<SOGameEvent, List<GameObject>>();
-        public GameEventListener[] eventObjects;
+
+        private List<GameObject> unassignedEventListeners = new List<GameObject>();
+
+        private GameEventListener[] eventObjects;
+
+        private Vector2 scrollPos;
+
+        private int toolbarInt = 0;
+        private string[] toolbarStrings = { "Active Scene Events", "Unassigned Event Listeners" };
 
         [MenuItem("Window/EventManager")]
         public static void ShowWindow()
@@ -47,34 +55,61 @@ namespace ScriptableObjectEvent
         }
 
         private void OnGUI()
+        {          
+            toolbarInt = GUILayout.Toolbar(toolbarInt, toolbarStrings);
+
+            switch(toolbarInt)
+            {
+                case 0:
+                    DrawActiveSceneEvents();
+                    break;
+                case 1:
+                    DrawUnassignedEvents();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Draw editor window showing all the active events being used in the scene
+        /// </summary>
+        private void DrawActiveSceneEvents()
         {
             Rect r = position; //position of the window - can get the height and width from here
             float windowWidth = position.width / 4; //Width of each section
 
             GUIStyle centerLabelStyle = new GUIStyle();
-            centerLabelStyle.alignment = TextAnchor.MiddleCenter;
+            centerLabelStyle.alignment = TextAnchor.MiddleLeft;
             centerLabelStyle.fontStyle = FontStyle.Bold;
 
-            GUILayout.Label("Event Manager", EditorStyles.boldLabel);
+            GUIStyle midLeftAlign = new GUIStyle();
+            midLeftAlign.alignment = TextAnchor.MiddleLeft;
+            midLeftAlign.wordWrap = true;
+
+            EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+            GUILayout.Label("Show all events being used in the current scene. \nWhile the scene is playing you can also raise any of the events.", EditorStyles.wordWrappedMiniLabel);
+            EditorGUILayout.EndHorizontal();
 
             EditorGUILayout.BeginHorizontal(EditorStyles.helpBox); //All the Event names
             GUILayout.Label("Event Name", centerLabelStyle, GUILayout.Width(windowWidth));
             GUILayout.Label("Description", centerLabelStyle, GUILayout.Width(windowWidth));
             GUILayout.Label("GameObject", centerLabelStyle, GUILayout.Width(windowWidth));
             GUILayout.Label("Raise Event", centerLabelStyle, GUILayout.Width(windowWidth));
+
             EditorGUILayout.EndHorizontal();
+
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
 
             //Print out a list of all the Events and each GameObject thats part of the event
             foreach (SOGameEvent key in gameEventDictionary.Keys)
             {
                 //***********************Event Name***********************
                 EditorGUILayout.BeginHorizontal(EditorStyles.helpBox); //All the Event names
-                GUILayout.Label(key.name, GUILayout.Width(windowWidth));
+                GUILayout.Label(key.name, midLeftAlign, GUILayout.Width(windowWidth));
 
                 //***********************Event Description***********************
                 EditorGUILayout.BeginVertical(GUILayout.MinWidth(windowWidth));
                 GUI.skin.label.wordWrap = true; //Wrap the label text
-                GUILayout.Label(key.eventDescription, GUILayout.Width(windowWidth));
+                GUILayout.Label(key.eventDescription, midLeftAlign, GUILayout.Width(windowWidth));
                 EditorGUILayout.EndVertical();
                 //***************************************************************
 
@@ -82,15 +117,14 @@ namespace ScriptableObjectEvent
                 EditorGUILayout.BeginVertical(GUILayout.ExpandWidth(true)); //All objects that use the Event
                 foreach (GameObject value in gameEventDictionary[key])
                 {
-                    GUILayout.Label(value.name, GUILayout.Width(windowWidth)); //Gameobject names
-                    EditorGUILayout.ObjectField(value, typeof(object), true, GUILayout.Width(windowWidth));
+                    GUILayout.Label(value.name, midLeftAlign, GUILayout.Width(windowWidth)); //Gameobject names
+                    EditorGUILayout.ObjectField(value, typeof(object), true, GUILayout.Width(windowWidth - 10));
                 }
                 EditorGUILayout.EndVertical();
                 //***************************************************************
 
                 //***********************Event Raise***********************
                 EditorGUILayout.BeginVertical(); //Button to trigger all the events 
-
                 if (EditorApplication.isPlaying) //If the application is playing - draw the raise buttons
                 {
                     if (GUILayout.Button("Raise Event"))
@@ -103,6 +137,28 @@ namespace ScriptableObjectEvent
 
                 EditorGUILayout.EndHorizontal(); //End of the Event names
             }
+
+            EditorGUILayout.EndScrollView();
+        }
+
+        /// <summary>
+        /// Draw editor window showing all the unassigned GameEvent Listener components
+        /// </summary>
+        private void DrawUnassignedEvents()
+        {
+            EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+            GUILayout.Label("Show all GameObjects that have the GameEvent Listener component, but haven't assigned a GameEvent to it.", EditorStyles.wordWrappedMiniLabel);
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginVertical(GUILayout.ExpandWidth(true)); //All objects that use the Event
+            foreach (GameObject value in unassignedEventListeners)
+            {
+                GUILayout.Label(value.name); //Gameobject names
+                EditorGUILayout.ObjectField(value, typeof(object), true);
+            }
+            EditorGUILayout.EndVertical();
+
+            Repaint(); //Repaint incase the GameEventListener is assigned
         }
 
         /// <summary>
@@ -128,8 +184,8 @@ namespace ScriptableObjectEvent
         /// </summary>
         private void GetAllScriptableEventsInScene()
         {
-            //Clear the dictionary
-            ClearDictionary();
+            //Clear the dictionary and list
+            ClearCollections();
 
             //Get all the eventObjects in the scene
             eventObjects = FindObjectsOfType<GameEventListener>();
@@ -156,13 +212,23 @@ namespace ScriptableObjectEvent
                         gameEventDictionary[listener.Event] = copyList;
                     }
                 }
+                else //Add to the unassignedObject list
+                {
+                    unassignedEventListeners.Add(listener.gameObject);
+                }
             }
         }
 
-        private void ClearDictionary()
+        /// <summary>
+        /// Clear both the EventDictionary and UnassignedEventListeners
+        /// </summary>
+        private void ClearCollections()
         {
             //Empty the Dictionary
             gameEventDictionary.Clear();
+
+            //Empty the List
+            unassignedEventListeners.Clear();
         }
 
 #endif
